397b34623d22d4c72403e746d799bd42848f5504
salehmohamed99 <sma010174@gmail.com>
2026-05-23 15:31:38 +0300

    Update Address entity and database schema
    Replaced the `AreaAr` property in the `Address` entity with `City` and added a new `Area` property. Updated the `ApplicationDbContextModelSnapshot` to reflect these changes. Added migration files to rename the `AreaAr` column to `City` and add the `Area` column in the `Addresses` table.

--- Graduation-Domain/Entities/Address.cs
+++ Graduation-Domain/Entities/Address.cs
@@ -9,7 +9,9 @@ namespace Graduation_domain.Entities
         public ApplicationUser User { get; set; }
 
         [Required(ErrorMessage = "City is required")]
-        public string AreaAr { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+
+        public string Area { get; set; } = string.Empty;
         public string Street { get; set; } = string.Empty;
         public string BuildingNumber { get; set; } = string.Empty;
         public string Notes { get; set; } = string.Empty;
---
+++ Graduation-infrastructure/Migrations/20260523123048_edit.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Graduation_infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class edit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AreaAr",
+                table: "Addresses",
+                newName: "City");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Area",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Area",
+                table: "Addresses");
+
+            migrationBuilder.RenameColumn(
+                name: "City",
+                table: "Addresses",
+                newName: "AreaAr");
+        }
+    }
+}
