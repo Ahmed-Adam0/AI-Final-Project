a6ad42320e7ec47a3412656840b1c69e22261c31
salehmohamed99 <sma010174@gmail.com>
2026-05-30 01:12:53 +0300

    edits

--- Graduation-Application/IServices/INotificationService.cs
+++ Graduation-Application/IServices/INotificationService.cs
@@ -5,7 +5,7 @@ namespace Graduation_Application.IServices
     public interface INotificationService
     {
         Task SendNotificationAsync(string userId, string message);
-        Task SendOrderConfirmationAsync(string userId, int orderId);
+        Task SendOrderConfirmationAsync(string userId, int orderId, decimal totalPrice);
         Task SendOrderStatusUpdateAsync(string userId, int orderId, string newStatus);
         Task SendOrderCancellationAsync(string userId, int orderId);
     }
