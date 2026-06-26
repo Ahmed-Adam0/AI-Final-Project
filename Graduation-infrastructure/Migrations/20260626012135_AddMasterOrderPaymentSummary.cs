31d92a0fc1526e34ea1b50f5805ce49c85cd34eb
Sultan Ali <sultan.alhejeery@gmail.com>
2026-06-26 05:45:33 +0300

    push

---
+++ Graduation-infrastructure/Migrations/20260626012135_AddMasterOrderPaymentSummary.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Graduation_infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMasterOrderPaymentSummary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentStatus",
+                table: "Orders",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "Unpaid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentStatus",
+                table: "Orders");
+        }
+    }
+}
